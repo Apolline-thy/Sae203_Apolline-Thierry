7c66404b954ca8c4a4e0407f22a8609f6004e65e
Apolline Thierry <apolline.thierry@etu.unilim.fr>
2025-04-03 11:57:59 +0200

    maj DataMovie

---
+++ app/data/dataMovie.js
@@ -0,0 +1,23 @@
+// URL où se trouve le répertoire "server" sur mmi.unilim.fr
+let HOST_URL = "https://mmi.unilim.fr/~thierry18/SAE2.03"; //"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
+
+let DataMovie = {};
+
+/**
+ * Fetches data from the server based on the specified day.
+ *
+ * @param {string} week - The week parameter to be sent to the server.
+ * @param {string} day - The day parameter to be sent to the server.
+ * @returns The response from the server.
+ *
+ * DataMovie.request permet de récupérer des données depuis le serveur.
+ * Elle prend en paramètre une semaine (1, 2, ..., 52) et un jour (lundi mardi...)
+ * renvoie les données contenues dans la réponse du serveur (data).
+ */
+DataMovie.request = async function () {
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies");
+  let data = await answer.json();
+  return data;
+};
+
+export { DataMovie };
