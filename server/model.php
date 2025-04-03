0d4471032e1ca66a4d797b332080040266eee723
Apolline Thierry <apolline.thierry@etu.unilim.fr>
2025-04-03 10:37:53 +0200

    maj credentiel bdd

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "thierry18");
+define("DBLOGIN", "thierry18");
+define("DBPWD", "thierry18");
