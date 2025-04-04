4a1e344b943c178c3ac3fd833983cead2b581c49
Thierry Apolline <thierry18@mmi-dev.unilim.fr>
2025-04-04 10:44:33 +0200

    back itération2

--- admin/data/dataMovie.js
+++ admin/data/dataMovie.js
@@ -4,31 +4,31 @@ let HOST_URL = "https://mmi.unilim.fr/~thierry18/SAE2.03";//"http://mmi.unilim.f
 
 let DataMovie = {};
 
-DataMovie.add = async function (formData) {
-    try {
-        const response = await fetch(`${HOST_URL}/server/movies`, { // Adaptation de l'URL si nécessaire
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formData) // Conversion des données du formulaire au format JSON
-        });
-    
-        if (response.ok) {
-          const result = await response.json();
-          console.log("Le film a été ajouté avec succès :", result);
-          alert("Le film a été ajouté avec succès.");
-          return result;
-        } else {
-          const error = await response.json();
-          console.error("Erreur lors de l'ajout du film :", error);
-          alert("Erreur : " + error.message);
-          throw new Error(error.message);
-        }
-      } catch (err) {
-        console.error("Erreur de connexion au serveur :", err);
-        alert("Erreur de connexion au serveur : " + err.message);
-      }
+
+/** DataMenu.update
+ * 
+ * Prend en paramètre un objet FormData (données de formulaire) à envoyer au serveur.
+ * Ces données sont incluses dans une requête HTTP en méthode POST.
+ * Une requête POST au lieu de GET n'affiche pas les données dans l'URL (plus discret).
+ * Les données sont placées dans le corps (body) de la requête HTTP. Elles restent visibles mais
+ * en utilisant les outils de développement du navigateur (Network > Payload).
+ * La requête comprend aussi un paramètre todo valant update pour indiquer au serveur qu'il
+ * s'agit d'une mise à jour (car on a codé le serveur pour qu'il sache quoi faire en fonction de la valeur de todo).
+ * 
+ * @param {*} fdata un objet FormData contenant les données du formulaire à envoyer au serveur.
+ * @returns la réponse du serveur.
+ */
+DataMovie.add = async function (fdata) {
+    // fetch possède un deuxième paramètre (optionnel) qui est un objet de configuration de la requête HTTP:
+    //  - method : la méthode HTTP à utiliser (GET, POST...)
+    //  - body : les données à envoyer au serveur (sous forme d'objet FormData ou bien d'une chaîne de caractères, par exempe JSON)
+    let config = {
+        method: "POST", // méthode HTTP à utiliser
+        body: fdata // données à envoyer sous forme d'objet FormData
     };
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=add", config);
+    let data = await answer.json();
+    return data;
+}
 
 export {DataMovie};
