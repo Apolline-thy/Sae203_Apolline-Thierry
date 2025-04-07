721d55e38dbeecc1792a7936fb4d3b07d3b19b1b
Thierry Apolline <thierry18@mmi-dev.unilim.fr>
2025-04-07 14:25:20 +0200

    iteration 4

--- app/component/MovieCategory/script.js
+++ app/component/MovieCategory/script.js
@@ -1,20 +1,19 @@
-import {Movie} from "./component/Movie/script.js";
+import { Movie } from "../Movie/script.js";
 
 let templateFile = await fetch("./component/MovieCategory/template.html");
 let template = await templateFile.text();
 
-
 let MovieCategory = {};
 
-MovieCategory.format = function (film) {
-    let html = template; 
-   html = html.replace ("{{category}}", film.category);
-   html = html.replace ("{{cards}}", film.cards);
+MovieCategory.format = function (category) {
+    let categoryHtml = template;
+    categoryHtml = categoryHtml.replace("{{categoryName}}", category.name);
 
+    // Ensure category.movies is passed to Movie.format
+    let cardsHtml = Movie.format(category.movies || []);
+    categoryHtml = categoryHtml.replace("{{cards}}", cardsHtml);
 
-   return html;
-  }
+    return categoryHtml;
+};
 
 export { MovieCategory };
-
-
