b8ec08d6ee6fc616869d7364675d2dc61afb48b0
Thierry Apolline <thierry18@mmi-dev.unilim.fr>
2025-04-09 14:13:03 +0200

    changement pour chemins relatifs

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -10,14 +10,15 @@ NavBar.format = function (hAbout, profiles) {
   let options = `<option value="" data-age="0" data-img="">Choisir un profil</option>`;
   for (let i = 0; i < profiles.length; i++) {
     let p = profiles[i];
-    options += `<option value="${p.id}" data-age="0" data-img="https://mmi.unilim.fr/~thierry18/SAE2.03/server/images/${p.avatar}">${p.name}</option>`;
+    options += `<option value="${p.id}" data-age="0" data-img="../server/images/${p.avatar}">${p.name}</option>`;
   }
 
   console.log("Options générées :", options); // Vérifiez les options ici
 
-  let image = profiles[0]?.avatar
-    ? `https://mmi.unilim.fr/~thierry18/SAE2.03/server/images/${profiles[0].avatar}`
-    : "https://mmi.unilim.fr/~thierry18/SAE2.03/server/images/default-avatar.jpg";
+  // let image =
+  //   "https://mmi.unilim.fr/~thierry18/SAE2.03/server/images/default-avatar.jpg";
+
+  let image = "../server/images/default-avatar.jpg";
 
   html = html.replace("{{image}}", image);
   html = html.replace("{{profiles}}", options);
