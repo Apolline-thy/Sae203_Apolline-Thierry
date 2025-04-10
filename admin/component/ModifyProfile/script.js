3f9b4e316595c424f92c4e39739dcbc7203caadb
Thierry Apolline <thierry18@mmi-dev.unilim.fr>
2025-04-10 14:09:24 +0200

    iteration8

--- admin/component/ModifyProfile/script.js
+++ admin/component/ModifyProfile/script.js
@@ -1,19 +1,27 @@
 let templateFile = await fetch("./component/ModifyProfile/template.html");
 let template = await templateFile.text();
+let templateFile2 = await fetch(
+  "./component/ModifyProfile/templateProfile.html"
+);
+let templateProfile = await templateFile2.text();
 
 let ModifyProfile = {};
 
-ModifyProfile.format = function (handler, profiles) {
+ModifyProfile.format = function (profiles) {
   let html = template;
-  let options = `<option value="">Choisir un profil</option>`;
-  for (let i = 0; i < profiles.length; i++) {
-    let p = profiles[i];
-    options += `<option value="${p.id}" data-name="${p.name}" data-avatar="${p.avatar}" data-date_naissance="${p.date_naissance}">${p.name}</option>`;
+  let profilesHTML = "";
+
+  for (let p of profiles) {
+    let profile = templateProfile;
+    profile = profile.replace("{{name}}", p.name);
+    profile = profile.replace("{{id}}", p.id);
+
+    profilesHTML += profile;
   }
 
-  html = html.replace("{{handler}}", handler);
+  html = html.replace("{{handlerSelect}}", "C.handlerSelectProfile()");
   html = html.replace("{{handlerModify}}", "C.handlerProfileModify()");
-  html = html.replace("{{profiles}}", options);
+  html = html.replace("{{profiles}}", profilesHTML);
   return html;
 };
 
